1829cd0d0f4f5e948dccb3eac2e96722e2bfa72d
David Olowomeye <olowomeyedavid242@gmail.com>
2024-02-22 15:40:43 +0000

    Successfully implemented the feature that upload and save the blog banner image in AWS s3 Bucket

---
+++ src/common/aws.jsx
@@ -0,0 +1,31 @@
+import { useEffect } from "react";
+
+export const uploadImageFile = async (img) => {
+  try {
+    const response = await fetch("http://localhost:4000/upload-url");
+
+    if (!response.ok) {
+      throw new Error("Failed to fetch upload URL");
+    }
+
+    const { uploadUrl } = await response.json();
+
+    const uploadResponse = await fetch(uploadUrl, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+      body: img,
+    });
+
+    if (!uploadResponse.ok) {
+      throw new Error("Failed to upload image");
+    }
+
+    const imgUrl = uploadUrl.split("?")[0];
+    return imgUrl;
+  } catch (error) {
+    console.log("Error uploading image:", error);
+    return null;
+  }
+};
--- src/pages/Blog/CreatePost.jsx
+++ src/pages/Blog/CreatePost.jsx
@@ -1,19 +1,42 @@
-import React from "react";
+import React, { useRef } from "react";
 import "../../styles/pages/Blog/CreateBlog.css";
 import defaultBanner from "../../assets/pages/blog/blog banner.png";
+import { uploadImageFile } from "../../common/aws";
+import { Toaster, toast } from "react-hot-toast";
 
 const CreatePost = () => {
+  let blogBannerRef = useRef();
+
   const handleBannerUpload = (e) => {
     let img = e.target.files[0];
-    console.log(img);
+    // console.log(img);
+
+    let loadingToast = toast.loading("🔃 Uploading image...");
+
+    if (img) {
+      uploadImageFile(img)
+        .then((url) => {
+          if (url) {
+            toast.dismiss(loadingToast);
+            toast.success("Image uploaded successfully! 👍");
+            blogBannerRef.current.src = url;
+          }
+        })
+        .catch((err) => {
+          toast.dismiss(loadingToast);
+          return toast.error(err);
+        });
+    }
   };
 
   return (
     <section>
       <div className="create-cover">
         <div className="create-container">
+          <Toaster />
+
           <label htmlFor="uploadBanner">
-            <img src={defaultBanner} alt="" />
+            <img ref={blogBannerRef} src={defaultBanner} alt="" />
             <input
               type="file"
               id="uploadBanner"
